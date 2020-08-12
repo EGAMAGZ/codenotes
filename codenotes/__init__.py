f8cdd94c9444e640a80ca82217b02410578cf64b
EGAMAGZ <gazapito1@hotmail.com>
2020-08-11 19:15:46 -0500

    *Modified .gitignore *Added more arguments to search subparser

--- codenotes/__init__.py
+++ codenotes/__init__.py
@@ -6,7 +6,7 @@ __version__ = '0.0.1'
 
 def main():
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog='codenotes')
     parser.add_argument('--version', '-v', action='version', version=__version__)
     subparsers = parser.add_subparsers(dest='subargs')
 
@@ -16,6 +16,16 @@ def main():
 
     search = subparsers.add_parser('search')
     search.add_argument('type', choices=['note', 'todo'])
-    search.add_argument('text', type=str)
+    search.add_argument('text', action='store', nargs='?')
+    search_group = search.add_mutually_exclusive_group()
+    search_group.add_argument('--today', '-t', action='store_true')
+    search_group.add_argument('--yesterday', '-y', action='store_true')
+    search_group.add_argument('--week', '-w', action='store_true')
+    search_group.add_argument('--month', '-m', action='store_true')
 
     args = parser.parse_args()
+
+    if args.subargs == 'add':
+        pass
+    elif args.subargs == 'search':
+        pass
