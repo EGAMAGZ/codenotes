989365b5f2b50bc0da48a9f228337f15d9d08a3b
EGAMAGZ <gazapito1@hotmail.com>
2020-08-22 06:59:41 -0500

    test_todo.py's unittest classes compress in just one

--- tests/console/test_todo.py
+++ tests/console/test_todo.py
@@ -5,34 +5,25 @@ from codenotes.console.todo import AddTodo
 from codenotes import parse_args
 
 
-class TestAddOneTodo(unittest.TestCase):
-    def setUp(self) -> None:
+class TestAddTodo(unittest.TestCase):
+
+    def test_add_one_todo(self):
         args = parse_args(['add', 'todo', 'New todo task #1'])
-        self.add_todo = AddTodo(args)
+        add_todo = AddTodo(args)
+        self.assertTrue(isinstance(add_todo.todo_task, str))
+        self.assertEqual(add_todo.todo_task, 'New todo task #1')
 
-    def test_todo_text(self):
-        self.assertTrue(isinstance(self.add_todo.todo_task, str))
-        self.assertEqual(self.add_todo.todo_task, 'New todo task #1')
-
-
-class TestAddManyTodos(unittest.TestCase):
-    def setUp(self) -> None:
+    def test_add_many_todos(self):
         args = parse_args(['add', 'todo', 'New todo task #1;New todo task #2'])
-        self.add_todo = AddTodo(args)
+        add_todo = AddTodo(args)
+        self.assertTrue(isinstance(add_todo.todo_task, List))
+        self.assertListEqual(add_todo.todo_task, ['New todo task #1', 'New todo task #2'])
 
-    def test_todo_text(self):
-        self.assertTrue(isinstance(self.add_todo.todo_task, List))
-        self.assertListEqual(self.add_todo.todo_task, ['New todo task #1', 'New todo task #2'])
-
-
-class TestAddBadInputTodo(unittest.TestCase):
-    def setUp(self) -> None:
+    def test_add_bad_input_todo(self):
         args = parse_args(['add', 'todo', ';'])
-        self.add_todo = AddTodo(args)
-
-    def test_todo_text(self):
-        self.assertTrue(isinstance(self.add_todo.todo_task, List))
-        self.assertListEqual(self.add_todo.todo_task, [])
+        add_todo = AddTodo(args)
+        self.assertTrue(isinstance(add_todo.todo_task, List))
+        self.assertListEqual(add_todo.todo_task, [])
 
 
 if __name__ == '__main__':
