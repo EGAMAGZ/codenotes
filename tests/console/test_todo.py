1b8f32b395eb1dcda3e393c597a83314d428984c
EGAMAGZ <gazapito1@hotmail.com>
2020-08-22 20:11:25 -0500

    Commented test_todo.py

--- tests/console/test_todo.py
+++ tests/console/test_todo.py
@@ -9,20 +9,26 @@ from codenotes import parse_args
 class TestAddTodo(unittest.TestCase):
 
     def test_add_one_todo(self):
+        """ Test the storage of one task """
         args = parse_args(['add', 'todo', 'New todo task #1'])
         add_todo = AddTodo(args)
+
         self.assertTrue(isinstance(add_todo.todo_task, str))
         self.assertEqual(add_todo.todo_task, 'New todo task #1')
 
     def test_add_many_todos(self):
+        """ Test the storage of two tasks """
         args = parse_args(['add', 'todo', 'New todo task #2;New todo task #3'])
         add_todo = AddTodo(args)
+
         self.assertTrue(isinstance(add_todo.todo_task, List))
         self.assertListEqual(add_todo.todo_task, ['New todo task #2', 'New todo task #3'])
 
     def test_add_bad_input_todo(self):
+        """ Test bad task input, when is only typed ; """
         args = parse_args(['add', 'todo', ';'])
         add_todo = AddTodo(args)
+
         self.assertTrue(isinstance(add_todo.todo_task, List))
         self.assertListEqual(add_todo.todo_task, [])
 
@@ -33,33 +39,41 @@ class TestSearchTodo(unittest.TestCase):
         self.date = datetime.now().date().strftime('%Y-%m-%d')
 
     def test_search_text_date(self):
+        """ Test that search only one task by keywords and date in common """
+        # Stores a fourth task
         args = parse_args(['add', 'todo', 'Different','task'])
         AddTodo.set_args(args)
 
         expected_tasks=[('Different task', 0, self.date)]
         args = parse_args(['search', 'todo', 'Different', '--today'])
         query = SearchTodo(args).sql_query()
+
         self.assertCountEqual(query, expected_tasks)
 
     def test_search_text_todo(self):
+        """ Test that search all tasks that match to some keywords  """
         expected_tasks = [
             ('New todo task #1', 0, self.date),
             ('New todo task #2', 0, self.date),
             ('New todo task #3', 0, self.date)
         ]
+
         args = parse_args(['search', 'todo', 'New', 'todo'])
         query = SearchTodo(args).sql_query()
         self.assertCountEqual(query, expected_tasks)
 
     def test_search_today_todo(self):
+        """ Test that search for the four tasks added """
         expected_tasks = [
             ('New todo task #1', 0, self.date),
             ('New todo task #2', 0, self.date),
             ('New todo task #3', 0, self.date),
             ('Different task', 0, self.date)
         ]
+
         args = parse_args(['search', 'todo', '--today'])
         query = SearchTodo(args).sql_query()
+
         self.assertCountEqual(query, expected_tasks)
 
     def tearDown(self) -> None:
