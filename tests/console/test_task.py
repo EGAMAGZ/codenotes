13b8bf59ecf594507a6b24c3956e9d44be37f28e
EGAMAGZ <gazapito1@hotmail.com>
2020-08-28 19:17:08 -0500

    Created tests to search tasks by month and a day before

--- tests/console/test_task.py
+++ tests/console/test_task.py
@@ -8,13 +8,13 @@ from codenotes import parse_args
 
 class TestAddTodo(unittest.TestCase):
 
-    def test_add_one_task(self):
-        """ Test the storage of one task """
-        args = parse_args(['add', 'task', 'New task #1'])
+    def test_add_bad_input_task(self):
+        """ Test bad task input, when is only typed ; """
+        args = parse_args(['add', 'task', ';'])
         add_task = AddTask(args)
 
-        self.assertTrue(isinstance(add_task.task, str))
-        self.assertEqual(add_task.task, 'New task #1')
+        self.assertTrue(isinstance(add_task.task, List))
+        self.assertListEqual(add_task.task, [])
 
     def test_add_many_tasks(self):
         """ Test the storage of two tasks """
@@ -24,13 +24,13 @@ class TestAddTodo(unittest.TestCase):
         self.assertTrue(isinstance(add_task.task, List))
         self.assertListEqual(add_task.task, ['New task #2', 'New task #3'])
 
-    def test_add_bad_input_task(self):
-        """ Test bad task input, when is only typed ; """
-        args = parse_args(['add', 'task', ';'])
+    def test_add_one_task(self):
+        """ Test the storage of one task """
+        args = parse_args(['add', 'task', 'New task #1'])
         add_task = AddTask(args)
 
-        self.assertTrue(isinstance(add_task.task, List))
-        self.assertListEqual(add_task.task, [])
+        self.assertTrue(isinstance(add_task.task, str))
+        self.assertEqual(add_task.task, 'New task #1')
 
 
 class TestSearchTodo(unittest.TestCase):
@@ -39,11 +39,25 @@ class TestSearchTodo(unittest.TestCase):
         self.date = datetime.now().date().strftime('%Y-%m-%d')
         self.default_category_name = 'TODO Tasks'
 
-    def test_search_text_date(self):
-        """ Test that search only one task by keywords and date in common """
+    def test_search_month_task(self):
         # Stores a fourth task
         args = parse_args(['add', 'task', 'Different', 'task'])
         AddTask.set_args(args)
+
+        expected_tasks = [
+            ('New task #1', 1, self.date, self.default_category_name),
+            ('New task #2', 1, self.date, self.default_category_name),
+            ('New task #3', 1, self.date, self.default_category_name),
+            ('Different task', 1, self.date, self.default_category_name)
+        ]
+
+        args = parse_args(['search', 'task', '--month'])
+        query = SearchTask(args).sql_query()
+
+        self.assertCountEqual(query, expected_tasks)
+
+    def test_search_text_date(self):
+        """ Test that search only one task by keywords and date in common """
 
         expected_tasks=[('Different task', 1, self.date, self.default_category_name)]
         args = parse_args(['search', 'task', 'Different', '--today'])
@@ -60,6 +74,7 @@ class TestSearchTodo(unittest.TestCase):
         ]
         args = parse_args(['search', 'task', 'New', 'task'])
         query = SearchTask(args).sql_query()
+
         self.assertCountEqual(query, expected_tasks)
 
     def test_search_today_task(self):
@@ -72,6 +87,14 @@ class TestSearchTodo(unittest.TestCase):
         ]
 
         args = parse_args(['search', 'task', '--today'])
+        query = SearchTask(args).sql_query()
+
+        self.assertCountEqual(query, expected_tasks)
+
+    def test_search_yesterday_task(self):
+        expected_tasks = []
+
+        args = parse_args(['search', 'task', '--yesterday'])
         query = SearchTask(args).sql_query()
 
         self.assertCountEqual(query, expected_tasks)
