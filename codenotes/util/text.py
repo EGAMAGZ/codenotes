c5181d5f37a6974addcf1faef9fb747c837d0079
EGAMAGZ <gazapito1@hotmail.com>
2021-01-03 21:19:27 -0600

    Commented text.py

--- codenotes/util/text.py
+++ codenotes/util/text.py
@@ -3,12 +3,27 @@ from typing import overload, List, Union
 from codenotes.util.args import format_argument_text
 
 
-def text_break(text: str, max_length: int = 15) -> str:
+def text_break(complete_text: str, max_length: int = 15) -> str:
+    """ Functions that breaks the text you pass after a defined length (Default 15 characters)
 
-    if len(text) > 15:
-        return f'{text[:max_length]}...'
+    Parameters
+    ----------
+    complete_text: str
+        Whole text before breaking it
+
+    max_length: int
+        Max length to break the text and add to the ending '...'
+
+    Returns
+    -------
+    text_breaked: str
+        Depending in the length of the text, it's the way the text will be broken
+    """
+
+    if len(complete_text) > 15:
+        return f'{complete_text[:max_length]}...'
     else:
-        return text
+        return complete_text
 
 @overload
 def format_task_text(text: List[str]) -> List[str]: ...
@@ -18,7 +33,7 @@ def format_task_text(text: List[str]) -> List[str]: ...
 def format_task_text(text: List[str]) -> str: ...
 
 
-def format_task_text(text: List[str]) -> Union[List[str], str]:
+def format_task_text(task_text: List[str]) -> Union[List[str], str]:
     """ Function that formats text passed through arguments
 
     Parameters
@@ -28,12 +43,10 @@ def format_task_text(text: List[str]) -> Union[List[str], str]:
     
     Returns
     -------
-    task_text : str
-        Task of text passed in arguments and joined
-    tasks_list : List[str]
-        List of texts of task joined and stripped
+    tasks_list : Union[List[str], str]
+        List of texts of task joined and stripped or, task of text passed in arguments and joined
     """
-    task_text = format_argument_text(text)
+    task_text = format_argument_text(task_text)
 
     if ';' in task_text:
         tasks_list = []
