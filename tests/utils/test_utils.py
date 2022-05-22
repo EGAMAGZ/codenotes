9b71d0b7535870d565ab0d1185f70fd18d97e0cc
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2022-05-21 19:35:01 -0500

    Fixed assertion error

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -2,7 +2,7 @@ from codenotes import Annotations
 
 
 def test_annotation_that_exist():
-    assert Annotations.get_value_by_key("task") == -1
+    assert Annotations.get_value_by_key("task") != -1
     assert Annotations.get_value_by_key("TaSk") != -1
 
 
